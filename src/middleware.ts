3bf97b4abe561345811eea80ab75b825c22f2b5a
sushilkafle99 <sushil.kafle@riacollege.edu.np>
2026-06-04 11:26:42 +0545

    change

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { verifyToken } from "@/utils/auth";
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // Retrieve token from cookies
